bcefe71ef155d119e58e2b71bae8f9c7eddbf218
jorge <yo_coqui@hotmail.com>
2018-08-03 22:28:15 -0400

    subiendo el codigo de hola

--- src/main/java/com/DevOps/demo/DemoApplication.java
+++ src/main/java/com/DevOps/demo/DemoApplication.java
@@ -9,5 +9,6 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 		System.out.println("<<<< Mundo >>>>");
+		System.out.println("<<<< Hola >>>>");
 	}
 }
